0ec158a17d3a2cb28ac498dcde0e844c686115e7
anmolagarwal <anmol.agarwal2004@yahoo.com>
2022-01-13 00:52:08 +0530

    pen ,nep value sorted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import CurrencyRow from "./CurrencyRow";
 
+const BASE_URL = "https://api.exchangerate.host/latest";
 function App() {
+  // we want to use useState to state which currency will be converted
+  // we dont have any initial state so we put empty array
+  const [currencyOptions, currencySelector] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+  // we use useEffect hook to fetch data and return to empty array so it render once
+  // we can not use async/await so use .then which will return a promise and then
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+       // we pick the first currency in the data.rates array
+      .then((data) => {
+        const initialCurrency = Object.keys(data.rates)[0];
+        currencySelector([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(initialCurrency);
+        setExchangeRate(data.rates[initialCurrency]);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]));
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Converter Form</h1>
+      {/* pass currency state as prop*/}
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+    </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function CurrencyRow(props) {
+    // destructure props 
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount,
+  } = props;
+  return (
+    <div>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+        min="0.01"
+		step="0.01"
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map((option) => (
+          <option key={{option}+{option}} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
